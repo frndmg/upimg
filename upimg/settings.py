64ab4355f20f11f0783a7485dbf625044fcd3ec2
Fernando Martinez <frndmartinezglez@gmail.com>
2019-08-19 21:07:08 -0400

    Configure rest framework

--- upimg/settings.py
+++ upimg/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'minio',
 
     'handler',
@@ -120,6 +121,17 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Rest framework settigns
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Minio setings
