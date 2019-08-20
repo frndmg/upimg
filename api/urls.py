7db81ea46a9c554321c81f228b1a89f52c6c53fd
Fernando Martinez <frndmartinezglez@gmail.com>
2019-08-20 15:25:58 -0400

    Remove unused router

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 
-from rest_framework.routers import DefaultRouter
-
 from . import views
-
-router = DefaultRouter()
 
 urlpatterns = [
     path('upload_link', views.upload_link, name='upload_link'),
     path('upload/<slug:token>', views.upload_image, name='upload'),
     path('<int:pk>', views.get_image, name='get_image'),
     path('statistics', views.statistics, name='statistics'),
-] + router.urls
+]
