907788482c908ffa886ae4dab7fb4166bb637b09
Fernando Martinez <frndmartinezglez@gmail.com>
2019-08-20 14:15:56 -0400

    Remove unused comment

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-# router.register('', views.ImageViewSet, basename='images')
 
 urlpatterns = [
     path('upload_link', views.upload_link, name='upload_link'),
